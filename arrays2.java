371ac287ffeee936098f33c54d1e3da96c6008c6
souzzays <souzafreitas4165@gmail.com>
2025-08-25 21:49:31 -0300

    arrays2.java

--- arrays2.java
+++ arrays2.java
@@ -1,6 +1,6 @@
 public class arrays2 {
     public static void main(String[] args) {
-        int[] num = {12, 6, 15, 23, 10, 33, 58, 47, 18, 8};
+        int[] num = {13, 7, 15, 23, 21, 33, 59, 40, 20, 8};
 
         System.out.println("numeros pares: ");
         for (int i = 0; i < num.length; i++){
@@ -10,3 +10,4 @@ public class arrays2 {
         }
     }
 }
+
